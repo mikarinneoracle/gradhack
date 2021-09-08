e2d6c4fadf1d0c135c78844a66ea72c94ad06fd4
Mika Rinne <mika.rinne@oracle.com>
2021-09-08 16:09:01 +0300

    create

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -f gradhack-1.0.0.tgz
+npm run bots-node-sdk -- pack
---
+++ components/sites.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const request = require('request');
+
+// You can use your favorite http client package to make REST calls, however, the node fetch API is pre-installed with the bots-node-sdk.
+// Documentation can be found at https://www.npmjs.com/package/node-fetch
+// Un-comment the next line if you want to make REST calls using node-fetch.
+// const fetch = require("node-fetch");
+
+module.exports = {
+  metadata: () => ({
+    name: 'sites',
+    properties: {
+       url: { required: true, type: 'string' },
+       token: { required: true, type: 'string' },
+       siteid: { required: true, type: 'string' },
+    },
+    supportedActions: ['next', 'error', 'tryagain']
+  }),
+  invoke: (context, done) => {
+    const { url } = context.properties();
+    const { token } = context.properties();
+    var { siteid } = context.properties();
+    var id = JSON.parse(siteid);
+    var msg = "";
+
+    context.logger().info(siteid);
+    context.logger().info(url);
+    context.logger().info(token);
+
+    request({
+            followAllRedirects: true,
+            url: url,
+            method: "GET",
+            headers: {
+              'Content-Type': 'application/json',
+              'Api_token': token,
+              'p_site_id': id.number
+            }
+          }, function (error, response, body){
+          if (error) {
+            context.logger().info("Error: " + error);
+            context.reply(error);
+            context.keepTurn(true);
+            context.transition('error');
+            context.error(true);
+          } else {
+                var bodyResponse = JSON.parse(body);
+                 context.logger().info("Data received: \n" + JSON.stringify(bodyResponse));
+                 if(bodyResponse.items)
+                 {
+                     if(bodyResponse.items.length == 0)
+                     {
+                         msg = "Site not found with id " + id.number;
+                         context.logger().info(msg);
+                         context.reply(msg);
+                         context.keepTurn(true);
+                         context.transition('tryagain');
+                         context.error(false);
+                     } else {
+                         var item = bodyResponse.items[0];
+                         msg = "Details for the " + item.site_name + " are:\n" +
+                                        "id: " + item.site_id + "\n" +
+                                        "name: " + item.site_name + "\n" +
+                                        "city " + item.site_city + "\n" +
+                                        "country: " + item.site_country_code + "\n" +
+                                        "lat: " + item.lat + "\n" +
+                                        "long: " + item.long_ + "\n" +
+                                        "status: " + item.site_status + "\n" +
+                                        "aqi: " + item.aqi + "\n" +
+                                        "co: " + item.co + "\n" +
+                                        "sync date: " + item.sync_date + "\n";
+                         context.logger().info(msg);
+                         context.reply(msg);
+                         context.keepTurn(true);
+                         context.transition('next');
+                         context.error(false);
+                     }
+                 } else {
+                   context.reply("Error receiving a valid response from url " + url);
+                   context.keepTurn(true);
+                   context.transition('error');
+                   context.error(true);
+                 }
+          }
+          done();
+    });
+  }
+};
